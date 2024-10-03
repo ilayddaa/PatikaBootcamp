f828f3a46ff9f2e0870a1a930dd18ce9ea53a8cf
ilayda <ilydatass@gmail.com>
2024-10-04 01:55:17 +0300

    init

---
+++ week1/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace week
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.Write("Lütfen bir sayı giriniz: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 10)
+            {
+                Console.WriteLine("Sayı 10'dan büyüktür.");
+            }
+            
+            else if (number < 10)
+                Console.WriteLine("Sayı 10'dan küçüktür.");
+            
+            else
+            {
+                Console.WriteLine("Sayı 10'a eşittir.");
+            }
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Çift sayıdır.");
+            }
+
+            else
+            {
+                Console.WriteLine("Tek sayıdır.");
+
+            }
+            }
+
+            }
+                   
+            }
+
