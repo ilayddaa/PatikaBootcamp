a699a78fd5fd008cac51093838edce837925ea0e
ilayda <ilydatass@gmail.com>
2024-10-03 19:26:37 +0300

    init
    init

---
+++ week1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+namespace week
+{
+internal class Program
+{
+static void Main(string[]args)
+{
+
+
+Console.WriteLine("Lütfen Aşağıdaki Bilgileri Giriniz");
+Console.Write("TC Kimlik Numaranızı Girin:");
+string a = Console.ReadLine();
+Console.Write("Adı:");
+string b = Console.ReadLine();
+Console.Write("Soyadı:");
+string c= Console.ReadLine();
+Console.Write("Telefon Numarası:");
+string d= Console.ReadLine();
+Console.Write("Yaş:");
+string e= Console.ReadLine();
+Console.Write("İlk Aldığı Ürünün Fiyatı:");
+int f=Convert.ToInt32(Console.ReadLine());
+Console.Write("İkinci Aldığı Ürünün Fiyatı:");
+int g= Convert.ToInt32(Console.ReadLine());
+int h = ((f + g)* 10)/100;
+
+Console.WriteLine(" --------------------------------------------");
+
+Console.WriteLine(a + " " + "TC Kimlik Numarali "+ b+" " + c + " İsimli kisi icin kayit olusturulmustur.");
+Console.WriteLine(d + " " + "Telefon numarasina bildirim mesaji gonderilmistir.");
+Console.WriteLine(  (f + g) + " " + "Toplam harcama karsiligi kazanilan %10 patika puan miktari " + h +" TL'dir" );
+
+}
+}
+}
+
---
+++ week1/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace new_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İlayda Tas");
+            Console.WriteLine("Patika'nin uzman kadrosu ve tecrübeli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim!");
+
+
+        }
+
+    }
+}
+
