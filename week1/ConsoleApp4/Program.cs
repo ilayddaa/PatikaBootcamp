13d4aff965a73bb232b585f0e10fbee7da18ccd8
ilayda <ilydatass@gmail.com>
2024-10-04 12:55:21 +0300

    init

---
+++ week1/ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Lyda Manavına Hoş Geldiniz!");
+
+Console.WriteLine("Elma = 100TL ");
+Console.WriteLine("Karpuz = 200TL");
+Console.WriteLine("Muz = 500TL");
+Console.WriteLine("Portakal = 300TL");
+Console.WriteLine("Diğer bütün meyveler = 2TL");
+
+
+Console.Write("Hangi Meyveyi Satın Almak İstersiniz?");
+Console.Write("(Elma/Karpuz/Muz/Portakal/Diğer): ");
+
+string meyve = Console.ReadLine().ToLower(); // ToLower: Buyuk harf kucuk harfe dikkat eder.
+
+
+int fiyat = 0;
+switch (meyve)
+{
+    case "elma":
+        fiyat = 100;
+        break;
+    case "karpuz":
+        fiyat = 200;
+        break;
+    case "muz":
+        fiyat = 500;
+        break;
+    case "portakal":
+        fiyat = 300;
+        break;
+    case "diğer":
+        fiyat = 2;
+        break;
+    default:
+        Console.WriteLine("Geçersiz seçim!");
+        break;
+
+
+}
+
+{
+    Console.WriteLine("Seçtiğiniz meyvenin fiyatı " + fiyat + " TL'dir.");
+}
+Console.ReadKey(); //bilgilendirme içermeyen yazı kapanır.
