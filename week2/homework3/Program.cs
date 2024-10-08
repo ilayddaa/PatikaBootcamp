37ffdd42b919cb487d400023852dd3fadb561c2e
ilayda <ilydatass@gmail.com>
2024-10-08 19:08:06 +0300

    Add homework3 project to week2 solution

---
+++ week2/homework3/Program.cs
@@ -0,0 +1,24 @@
+﻿int sayac = 0;
+Console.Write("Lütfen bir sayı giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım!");
+    sayac++;
+
+} while (sayac < limit); // limit değerine kadar döngü çalışır.
+
+//-----------------------------
+int sayac1 = 0;
+Console.WriteLine("Lütfen bir sayı giriniz: ");
+int limit1 = Convert.ToInt32(Console.ReadLine());
+while (sayac1 < limit1)
+{
+    Console.WriteLine("'Ben bir Patika'lıyım'");
+    sayac1++;
+}
+// while döngüsü ile yapılan işlem do-while döngüsü ile yapılan işlem aynıdır fakat do-while döngüsünde döngü en az bir kere çalışır .
+// çünkü do-while döngüsünde koşul en sonda kontrol edilir. while döngüsünde ise koşul en başta kontrol edilir.
+
+
