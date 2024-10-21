059c79433eda98d47572668a175801e37db5b2dc
ilayda <ilydatass@gmail.com>
2024-10-21 23:25:07 +0300

    homework2-Generic List 1

---
+++ week5/homework2- Generic List1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+List<string> davetliler = new List<string>();
+davetliler.Add("Sagopa KAJMER");
+davetliler.Add("Ceza");
+davetliler.Add("Ezhel");
+davetliler.Add("Şehinşah");
+davetliler.Add("Fuat Ergin");
+davetliler.Add("Tarkan");
+davetliler.Add("Funda Arar");
+
+Console.WriteLine("Davetliler: ");
+for (int i = 0; i < davetliler.Count; i++) // 0'dan başlayıp davetliler.Count'a kadar gider
+{
+    Console.WriteLine($"{i + 1}. {davetliler[i]}");
+}
+Console.ReadKey();
