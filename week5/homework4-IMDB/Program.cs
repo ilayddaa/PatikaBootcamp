d7c5d25429f7f0a88f19c0a015112a004a667d0d
ilayda <ilydatass@gmail.com>
2024-10-22 19:07:38 +0300

    init

--- week5/homework4-IMDB/Program.cs
+++ week5/homework4-IMDB/Program.cs
@@ -5,7 +5,7 @@ public class Movie
 {
     public string Name { get; set; }
     public double Score { get; set; }
-    public Movie(string name, double score)
+    public Movie(string name, double score) //constructor
     {
         this.Name = name;
         this.Score = score;
@@ -16,18 +16,18 @@ public class Program
     static void Main()
     {
         Console.Clear();
-        List<Movie> movies = new List<Movie>();
+        List<Movie> movies = new List<Movie>(); //  Movie tipinde bir liste oluşturur
         for (; ; ) //sonsuz döngü
         {
             Console.Write("Lütfen film adını giriniz: ");
             string movieName = Console.ReadLine();
             Console.Write("Lütfen film puanını giriniz: ");
             double movieScore = Convert.ToDouble(Console.ReadLine());
-            movies.Add(new Movie(movieName, movieScore));
+            movies.Add(new Movie(movieName, movieScore)); //movies listesine film adı ve puanı ekler
             Console.WriteLine("Başarıyla eklendi.");
             Console.Write("Başka bir film eklemek ister misiniz? (E/H): ");
             string answer = Console.ReadLine();
-            if (answer.ToUpper() != "E")
+            if (answer.ToUpper() != "E") //E girilmezse döngüden çıkar
             {
                 break;
             }
@@ -40,7 +40,7 @@ public class Program
         Console.WriteLine("4 ile 9 arasındaki filmler");
         foreach (var movie in movies)
         {
-            if (movie.Score > 4 && movie.Score < 9)
+            if (movie.Score > 4 && movie.Score < 9) //4 ile 9 arasındaki filmleri yazdırır
             {
                 Console.WriteLine($"Film Adı: {movie.Name} - Puanı: {movie.Score}");
             }
