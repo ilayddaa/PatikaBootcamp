5369c1a97ca602f9a1454e4e7defe396fb818f7f
ilayda <ilydatass@gmail.com>
2024-10-23 17:01:08 +0300

    homework5 Try-Catch

---
+++ week5/homework5-Try-Catch/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+bool gecerli = false; // girişin doğruluğunu kontrol etmek için bir değişken tanımladık
+while (!gecerli)
+{
+    try // hata verirse catch ile yakalıyoruz
+    {
+        Console.Write("Lütfen sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan sayı girişi alıyoruz
+        gecerli = true; // doğru giriş yapıldıysa döngüden çık
+        Console.WriteLine("Girdiğiniz sayının karesi: " + sayi * sayi); // girilen sayının karesini yazdırıyoruz
+    }
+    catch // hata verirse catch ile yakalıyoruz
+    {
+        Console.WriteLine("Hatalı bir giriş yaptınız, lütfen tekrar deneyiniz.");
+    }
+}
+Console.ReadKey();
